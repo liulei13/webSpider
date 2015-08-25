ed5616b5ae5f51b0d0b92a66f52242b85170d3f8
liulei <liulei13@mail.ustc.edu.cn>
2015-08-25 21:34:20 +0800

    complete match and findall

--- webSpider.py
+++ webSpider.py
@@ -11,19 +11,37 @@ type=sys.getfilesystemencoding()
 headers={'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/35.0.1916.153 Safari/537.36 SE 2.X MetaSr 1.0'}
 #html=requests.get('http://wenku.baidu.com/new?fr=home')
 file_url=open('D:/webSpider/url.txt')
-url=file_url.read()
-file_url.close()
-html=requests.get(url, headers=headers)
-html.encoding='utf-8'
-print(html.encoding)
+url=file_url.readline()
+i=0
+while url:
+    url=url.strip('\n')
+    html=requests.get(url, headers=headers)
+    html.encoding='utf-8'
+    #print(html.encoding)
+    dst='D:/webSpider/'+str(i)+'.html'
+    f=open(dst,'w',encoding='utf-8')
+    f.write(html.text)
+    f.close()
 
-f=open('D:/webSpider/test.html','w',encoding='utf-8')
-f.write(html.text)
-f.close()
+    #data mining
+    names=re.findall('<h1>(.*?)</h1>',html.text,re.S)
+    for eachName in names:
+        print(eachName)
+    description=re.findall('<meta name="description" content="(.*?)>',html.text,re.S)
+    print(description[0])
+    resume=re.findall('<div class="para">(.*?)</div>',html.text,re.S)
+    for eachResume in resume:
+        print(eachResume.strip('<(.*?)>'))
+
+
+    print('URL:'+url)
+    url=file_url.readline()
+    i=i+1
+file_url.close()
 #print(html.text)
-chinese=re.findall('color: #039;">(.*?)</a>',html.text,re.S)
-for each1 in chinese:
-    print(each1)
+#chinese=re.findall('color: #039;">(.*?)</a>',html.text,re.S)
+#for each1 in chinese:
+   # print(each1)
 
 
 
